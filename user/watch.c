f5601954cf20268e782292998c4a326f34d037e3
Jakub Kliszewski <jakliszewski@gmail.com>
2025-10-24 15:12:12 +0200

    Fixed buggs in watch command.

--- user/watch.c
+++ user/watch.c
@@ -9,17 +9,21 @@
     int pause*/
 void watch(char *cmd[], int sleep) {
     int pid;
+    int ret;
     while(1) {
         pid = fork();
         if (pid == 0) {
             if(exec(cmd[0], cmd) < 0) exit(1);
         }
-        wait(&pid);
+        pid = wait(&ret);
+        if (!ret) {
+            fprintf(2, "%s failed\n", cmd[0]);
+            exit(1);
+        }
         pause(sleep);
     }
 }
-
-
+        
 int main(int argc, char *argv[]) {
     if(argc < 2) exit(1);
     if(strcmp(argv[1], "-n") == 0) {
