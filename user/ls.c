4624661a883827b7387a27b59eb8574e4b1cc803
Jakub Kliszewski <jakliszewski@gmail.com>
2025-11-07 01:04:27 +0100

    Working on ls queue

--- user/ls.c
+++ user/ls.c
@@ -22,11 +22,13 @@ char *fmtname(char *path) {
   return buf;
 }
 
-// Linked list for strings as values
-typedef struct node {
-  char        *dir;
-  struct node *next;
-} node_t;
+typedef struct {
+  char *arr[512];
+  int   front;
+  int   back;
+} Queue;
+
+int isFull(Queue *q) { return q->front >= 512; }
 
 // Add arg to ls
 // Use a queue (linked list) to add the dir files that need to get listed
@@ -36,7 +38,10 @@ void ls(char *path, char *arg) {
   int           fd;
   struct dirent de;
   struct stat   st;
-  node_t        queue = {0, 0};
+
+  Queue queue;
+  queue.front = -1;
+  queue.back = 0;
 
   if ((fd = open(path, O_RDONLY)) < 0) {
     fprintf(2, "ls: cannot open %s\n", path);
@@ -48,50 +53,37 @@ void ls(char *path, char *arg) {
     close(fd);
     return;
   }
-  if (strcmp(arg, "-R") == 0) {
-    switch (st.type) {
-    case T_DEVICE:
-      break;
-    case T_FILE:
-      printf("%s %d %d %d\n", fmtname(path), st.type, st.ino, (int)st.size);
-      break;
-    case T_DIR:
-      if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
-        printf("ls: path too long\n");
-        break;
-      } else {
-      }
-    }
-  } else {
-    switch (st.type) {
-    case T_DEVICE:
-    case T_FILE:
-      printf("%s %d %d %d\n", fmtname(path), st.type, st.ino, (int)st.size);
-      break;
 
-    case T_DIR:
-      if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
-        printf("ls: path too long\n");
-        break;
-      }
-      strcpy(buf, path);
-      p = buf + strlen(buf);
-      *p++ = '/';
-      while (read(fd, &de, sizeof(de)) == sizeof(de)) {
-        if (de.inum == 0)
-          continue;
-        memmove(p, de.name, DIRSIZ);
-        p[DIRSIZ] = 0;
-        if (stat(buf, &st) < 0) {
-          printf("ls: cannot stat %s\n", buf);
-          continue;
-        }
-        if (st.type == T_DIR)
-          printf("IS A DIR ");
-        printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, (int)st.size);
-      }
+  switch (st.type) {
+  case T_DEVICE:
+  case T_FILE:
+    printf("%s %d %d %d\n", fmtname(path), st.type, st.ino, (int)st.size);
+    break;
+  case T_DIR:
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+      printf("ls: path too long\n");
       break;
     }
+    strcpy(buf, path);
+    p = buf + strlen(buf);
+    *p++ = '/';
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+      if (de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if (stat(buf, &st) < 0) {
+        printf("ls: cannot stat %s\n", buf);
+        continue;
+      }
+      if (st.type == T_DIR)
+        printf("IS A DIR ");
+      printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, (int)st.size);
+      if (st.type == T_DIR && strcmp(arg, "-R") == 0)
+        // queue.arr =
+        printf("IS A DIR\n");
+    }
+    break;
   }
   close(fd);
 }
@@ -99,12 +91,12 @@ void ls(char *path, char *arg) {
 int main(int argc, char *argv[]) {
   int i;
   if (argc < 2) {
-    ls(".", " ");
+    ls(".", "");
     exit(0);
   }
   if (strcmp(argv[1], "-R") == 0) {
     if (argc < 3) {
-      ls(".", " ");
+      ls(".", "-R");
       exit(0);
     }
     for (i = 2; i < argc; i++) {
@@ -113,6 +105,6 @@ int main(int argc, char *argv[]) {
     }
   }
   for (i = 1; i < argc; i++)
-    ls(argv[i], " ");
+    ls(argv[i], "");
   exit(0);
 }
