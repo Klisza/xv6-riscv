efe56760ab44c13e523c639dee1db0e0d633964b
Jakub Kliszewski <jakliszewski@gmail.com>
2025-11-17 21:11:07 +0100

    Added sec checks to watch.

--- user/watch.c
+++ user/watch.c
@@ -6,29 +6,36 @@
     int fork
     int exec
     int wait
-    int pause*/
+    int pause */
 void watch(char *cmd[], int sleep) {
-    int pid;
-    int ret;
-    while(1) {
-        pid = fork();
-        if (pid == 0) {
-            if(exec(cmd[0], cmd) < 0) exit(1);
-        }
-        pid = wait(&ret);
-        if (!ret) {
-            fprintf(2, "%s failed\n", cmd[0]);
-            exit(1);
-        }
-        pause(sleep);
+  int pid;
+  int ret;
+  while (1) {
+    pid = fork();
+    if (pid == 0) {
+      if (exec(cmd[0], cmd) < 0)
+        exit(1);
     }
+    pid = wait(&ret);
+    if (pid < 0) {
+      fprintf(2, "Wait failed, there are no children.\n");
+      exit(1);
+    }
+    if (!ret) {
+      fprintf(2, "%s failed\n", cmd[0]);
+      exit(1);
+    }
+    if (!pause(sleep))
+      exit(1);
+  }
 }
-        
+
 int main(int argc, char *argv[]) {
-    if(argc < 2) exit(1);
-    if(strcmp(argv[1], "-n") == 0) {
-        watch(&argv[3], 20 * atoi(argv[2]));
-    }
-    else watch(&argv[1], 20);    
-    exit(0);
+  if (argc < 2)
+    exit(1);
+  if (strcmp(argv[1], "-n") == 0) {
+    watch(&argv[3], 20 * atoi(argv[2]));
+  } else
+    watch(&argv[1], 20);
+  exit(0);
 }
